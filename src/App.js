d2ef82b0648f44ca9567d8d6f0391be6a78ec7b1
Srishty Mangutte <srishtymangutte@gmail.com>
2023-02-02 20:59:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          
+          <LambdaDemo />
         </header>
       </div>
     )
